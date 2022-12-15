a537190a42ca85bd98174af5fe684522ca33464e
esmailalmoshki <93872469+esmailalmoshki@users.noreply.github.com>
2022-12-15 20:05:08 +0300

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="style.css" rel="stylesheet"/> 
+  <title><?php echo "Project" ?></title>
+</head>
+<body>
+  <nav>
+    <h1 class="header-text">Notify</h1>
+   <div class="links">
+      <a target="_blank" class="nav-link link" href="https://www.umassd.edu/dss/resources/students/classroom-strategies/why-take-notes/#:~:text=It%20helps%20you%20learn.,helps%20you%20keep%20a%20record.">Why note</a>
+   <a  class="nav-link link" href="https://www.youtube.com/watch?v=E7CwqNHn_Ns" target="_blank">How to note</a>
+  </div>
+  </nav>
+  
+  <article class="body-container">
+    <p class="article-text">The esiest-to-use digital note</p>
+    <img src="./images/note.jpeg" alt="digital note"/>
+    <a class="btn" href="./index2.php" >Make your notes</a>
+</article>
+  
+</body>
+</html>
